aa3c44e92f1718a8922608d213bca974eec50ab3
Jirayu Pharang <jirayu.ph@inet.co.th>
2020-03-26 23:12:20 +0700

    Test deploy

---
+++ precache-manifest.daa29c11d624ab06fe866b7f20bc874b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae826ca0229b68f34250b6b38db4a6ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eddc3afa53a9f3e4bf7",
+    "url": "/static/css/main.71d50405.chunk.css"
+  },
+  {
+    "revision": "14da5f6294082385fefc",
+    "url": "/static/js/2.d8b49a56.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d8b49a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eddc3afa53a9f3e4bf7",
+    "url": "/static/js/main.adabce68.chunk.js"
+  },
+  {
+    "revision": "ae16741e8ba08b365cf2",
+    "url": "/static/js/runtime-main.b32cd5ea.js"
+  },
+  {
+    "revision": "9a2be38156b189308db3b755e34d8d4d",
+    "url": "/static/media/ic-arrow-drop-down.9a2be381.svg"
+  },
+  {
+    "revision": "ba979780e3065c420f56a1f40c11fdba",
+    "url": "/static/media/ic-devices.ba979780.svg"
+  },
+  {
+    "revision": "4e658b8f573751c1eb648b9f676aac9f",
+    "url": "/static/media/ic-doc.4e658b8f.svg"
+  },
+  {
+    "revision": "eb5fc1bb19d757989ef7c0a63839d867",
+    "url": "/static/media/profile-lg.eb5fc1bb.jpg"
+  },
+  {
+    "revision": "26673714d202aa0091fa95fff3835a6c",
+    "url": "/static/media/profile.26673714.png"
+  }
+]);
